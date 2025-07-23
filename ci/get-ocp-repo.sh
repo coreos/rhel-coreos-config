78e697e8f1444e87574631bf43a2543aa431509e
Joel Capitao <jcapitao@redhat.com>
2025-07-23 15:23:45 +0200

    get-ocp-repo: pull the rhel-10 repos
    We need to pull the rhel10 repos since we added the PROW tests
    `rhcos-10-build-test-[metal|qemu]` with [1].
    Also, since we removed the Y stream from repo definitions in [2], we
    have to remove them also in our manifest.
    
    Finally, we can also enable rhel-10-early-kernel repo and pull
    ose-aws-ecr-image-credential-provider RPM.
    
    [1] https://github.com/openshift/release/pull/67305
    [2] https://github.com/openshift/release/pull/63561

--- ci/get-ocp-repo.sh
+++ ci/get-ocp-repo.sh
@@ -6,8 +6,9 @@ set -euo pipefail
 # content.
 
 urls=(
-    # theoretically that's the only one we need
+    # theoretically that's the only ones we need
     "http://base-4-19-rhel96.ocp.svc.cluster.local"
+    "http://base-4-20-rhel10.ocp.svc.cluster.local"
     # XXX: but also currently add 9.4 repos for crun-wasm when building extensions
     # https://github.com/openshift/os/issues/1680
     # https://github.com/openshift/os/pull/1682
@@ -16,8 +17,15 @@ urls=(
 )
 
 dest=$1; shift
+variant=$1; shift
 
 rm -f "$dest"
 for url in "${urls[@]}"; do
     curl --fail -L "$url" >> "$dest"
 done
+# One OCP release is tied to only one Y release of RHEL, so the ART team finds there is no value
+# specifying a Y stream in PROW. Then we have to add it back here to be consistent with our pipeline.
+# https://github.com/openshift/release/blob/master/core-services/release-controller/_repos/ocp-4.20-rhel10.repo
+if [[ "$variant" == rhel-10* ]]; then
+    sed -i "/^name =/s/rhel-10/$variant/;/^\[.*\]$/s/rhel-10/$variant/" "$dest"
+fi
