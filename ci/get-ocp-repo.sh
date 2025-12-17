bcf55c084824dc32c6098401efcd6ac7cb46e67d
Dusty Mabe <dusty@dustymabe.com>
2025-12-16 22:27:39 -0500

    ci: update repo config when running in prow
    We're now picking up just rhel98 and rhel101 repos for 4.22.
    
    We also now have 10.1 repos and not just 10 repos so we can drop the
    sed that we needed before to mangle the repo files.

--- ci/get-ocp-repo.sh
+++ ci/get-ocp-repo.sh
@@ -6,14 +6,8 @@ set -euo pipefail
 # content.
 
 urls=(
-    # theoretically that's the only ones we need
-    "http://base-4-19-rhel96.ocp.svc.cluster.local"
-    "http://base-4-20-rhel10.ocp.svc.cluster.local"
-    # XXX: but also currently add 9.4 repos for crun-wasm when building extensions
-    # https://github.com/openshift/os/issues/1680
-    # https://github.com/openshift/os/pull/1682
-    # https://issues.redhat.com/browse/COS-3075
-    "http://base-4-19-rhel94.ocp.svc.cluster.local"
+    "http://base-4-22-rhel98.ocp.svc.cluster.local"
+    "http://base-4-22-rhel101.ocp.svc.cluster.local"
 )
 
 dest=$1; shift
@@ -23,9 +17,3 @@ rm -f "$dest"
 for url in "${urls[@]}"; do
     curl --fail -L "$url" >> "$dest"
 done
-# One OCP release is tied to only one Y release of RHEL, so the ART team finds there is no value
-# specifying a Y stream in PROW. Then we have to add it back here to be consistent with our pipeline.
-# https://github.com/openshift/release/blob/master/core-services/release-controller/_repos/ocp-4.20-rhel10.repo
-if [[ "$variant" == rhel-10* ]]; then
-    sed -i "/^name =/s/rhel-10/$variant/;/^\[.*\]$/s/rhel-10/$variant/" "$dest"
-fi
